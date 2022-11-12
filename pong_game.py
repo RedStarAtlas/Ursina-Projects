7024a44c4632f760f5ddec392e36467499998edd
Madeline <redstaratlas@gmail.com>
2022-11-12 07:54:03 -0600

    pushing to save

--- pong_game.py
+++ pong_game.py
@@ -20,11 +20,26 @@ class Main(Entity):
     ball = Entity(model='circle', scale=.2, collider='box', speed=0, collision_cooldown=collision_cooldown)
     
     def update():
-        pass
+        ball.collision_cooldown -= time.dt
+        ball.position += ball.left * time.dt * ball.speed
+
+        left_paddle.y += (held_keys['w'] - held_keys['s']) * time.dt * 1
+        right_paddle.y += (held_keys['up arrow'] - held_keys['down arrow']) * time.dt * 1
     
+        if ball.collision_cooldown > 0:
+            return
+
+        hit_info = ball.intersects()
+        if hit_info.hit:
+            ball.collision_cooldown = collision_cooldown
+
+    def input(key):
+        if key == 'space':
+            info_text.enabled = False
+            reset()
     
-    def input():
-        pass
+        if key == 't':
+            ball.speed += 5
     
     app = Ursina(title='Pong Game!')
 
