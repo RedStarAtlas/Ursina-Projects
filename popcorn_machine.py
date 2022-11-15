c8193974debb5137034c36a6756fcb821945b115
Madeline <redstaratlas@gmail.com>
2022-11-15 07:42:56 -0600

    multiple are added

--- popcorn_machine.py
+++ popcorn_machine.py
@@ -2,30 +2,51 @@
 # A popcorn machine simulator
 
 from ursina import *
+import random
 
 app = Ursina()
 
-popcorn = Entity(model = 'sphere', texture = "images/popcorn.jpeg", scale = .5)
+popcorn_multiple = []
 
-popcorn.x = 0
-popcorn.y = 0
-popcorn.dx = 0.05
-popcorn.dy = 0.06
+for _ in range(40):
+    popcorn = Entity(model = 'sphere', texture = "images/popcorn.jpeg", scale = .3)
+
+    popcorn.x = random.randint(-3, 3)
+    popcorn.y = random.randint(-2, 2)
+    popcorn.z = random.randint(-4, 4)
+    popcorn.dx = random.randint(1, 10) / 100
+    popcorn.dy = random.randint(1, 10) / 100
+    popcorn.dz = random.randint(1, 10) / 100
+
+    popcorn_multiple.append(popcorn)
 
 def update():
-    popcorn.x += popcorn.dx
-    popcorn.y += popcorn.dy
-    if popcorn.x > 4:
-        popcorn.x = 4
-        popcorn.dx *= -1
-    if popcorn.x < -4:
-        popcorn.x = -4
-        popcorn.dx *= -1
-    if popcorn.y > 2:
-        popcorn.y = 2
-        popcorn.dy *= -1
-    if popcorn.y < -2:
-        popcorn.y = -2
-        popcorn.dy *= -1
+    for popcorn in popcorn_multiple:
+        popcorn.x += popcorn.dx
+        popcorn.y += popcorn.dy
+        popcorn.z += popcorn.dz
+
+        if popcorn.x > 3:
+            popcorn.x = 3
+            popcorn.dx *= -1
+        if popcorn.x < -3:
+            popcorn.x = -3
+            popcorn.dx *= -1
+
+        if popcorn.y > 2:
+            popcorn.y = 2
+            popcorn.dy *= -1
+        if popcorn.y < -2:
+            popcorn.y = -2
+            popcorn.dy *= -1
+
+        if popcorn.z > 4:
+            popcorn.z = 4
+            popcorn.dz *= -1
+        if popcorn.z < -4:
+            popcorn.z = -4
+            popcorn.dz *= -1
+
+        popcorn.rotation_z += 2
 
 app.run()
