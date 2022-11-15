be7ddd897a7041e947f44fdd568e4e8414f6949f
Madeline <redstaratlas@gmail.com>
2022-11-15 07:33:41 -0600

    got initial popcorn working. Next is to add multiple

--- popcorn_machine.py
+++ popcorn_machine.py
@@ -5,6 +5,27 @@ from ursina import *
 
 app = Ursina()
 
+popcorn = Entity(model = 'sphere', texture = "images/popcorn.jpeg", scale = .5)
+
+popcorn.x = 0
+popcorn.y = 0
+popcorn.dx = 0.05
+popcorn.dy = 0.06
+
 def update():
+    popcorn.x += popcorn.dx
+    popcorn.y += popcorn.dy
+    if popcorn.x > 4:
+        popcorn.x = 4
+        popcorn.dx *= -1
+    if popcorn.x < -4:
+        popcorn.x = -4
+        popcorn.dx *= -1
+    if popcorn.y > 2:
+        popcorn.y = 2
+        popcorn.dy *= -1
+    if popcorn.y < -2:
+        popcorn.y = -2
+        popcorn.dy *= -1
 
 app.run()
