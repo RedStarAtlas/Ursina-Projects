7f14d5adf83cf824577723c23b0ca9eac3fef033
Madeline <redstaratlas@gmail.com>
2022-11-15 09:13:52 -0600

    going to merge now

--- popcorn_machine.py
+++ popcorn_machine.py
@@ -6,26 +6,33 @@ import random
 
 app = Ursina()
 
+title = Text(text = "Working Popcorn Machine!", scale = 2, position = (-.8, .40), color=color.yellow)
+title_2 = Text(text = "Is there a purpose for this?", scale = 1, position = (-.8, .25))
+title_3 = Text(text = "Probably not.", scale = 1, position = (-.8, .20), color=color.yellow)
+title_4 = Text(text = "But now you've seen it!", scale = 1, position = (-.8, .15))
+title_5 = Text(text = "*This showcases the use of collision and gravity*", scale = 1, position = (-.8, -.20), color=color.yellow)
+title_6 = Text(text = "*Totally did not come up with this at midnight last night*", scale = .7, position = (-.8, -.30))
+# creates the multiple popcorn instead of a singular one
 popcorn_multiple = []
 
+# Adds gravity to the popcorn so that it falls instead of floats
 Gravity = -0.01
 
-machine_back = Entity(model="cube",  scale=(5, 6.5), position=(0, 0, 0), color=color.red)
-machine_front = Entity(model="cube",  scale=(4, 5), position=(0, 0, 0), color=color.white)
-label_box = Entity(model="cube",  scale=(3.8, .5), position=(0, 2.9, 0), color=color.yellow)
+machine_back = Entity(model="cube",  scale=(6.6, 8), position=(0, 0, 0), texture = "images/pop.jpg")
 
-# The camera angle the user sees the game as
+# The camera angle the user sees the popcorn machine
 camera.position = (-4, 0, -26)
 
-for _ in range(80):
+# The random allows the popcorn to move at random speeds instead of all the same speed
+for _ in range(150):
     popcorn = Entity(model = 'sphere', texture = "images/popcorn.jpeg", scale = .1)
 
     popcorn.x = random.randint(-2, 1)
-    popcorn.y = random.randint(-2, 2)
+    popcorn.y = random.randint(-3, 2)
     popcorn.z = random.randint(-4, 4)
-    popcorn.dx = random.randint(1, 8) / 200
-    popcorn.dy = random.randint(1, 8) / 200
-    popcorn.dz = random.randint(1, 8) / 200
+    popcorn.dx = random.randint(1, 10) / 150
+    popcorn.dy = random.randint(1, 10) / 150
+    popcorn.dz = random.randint(1, 10) / 150
 
     popcorn_multiple.append(popcorn)
 
@@ -46,8 +53,8 @@ def update():
         if popcorn.y > 2:
             popcorn.y = 2
             popcorn.dy *= -1
-        if popcorn.y < -2:
-            popcorn.y = -2
+        if popcorn.y < -3:
+            popcorn.y = -3
             popcorn.dy *= -1
 
         if popcorn.z > 4:
@@ -61,6 +68,7 @@ def update():
         popcorn.rotation_y += 2
         popcorn.rotation_z += 2
 
+# Allows the popcorn to collide into eachother 
     # Collision Checking
     for i in range(len(popcorn_multiple)):
         for j in range(i+1, len(popcorn_multiple)):
