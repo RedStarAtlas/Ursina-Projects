fa6148791f20358e932aaeb3c24e58bfc9e2a859
Madeline <redstaratlas@gmail.com>
2022-11-15 14:40:20 -0600

    initial commit

---
+++ hidden_object_game.py
@@ -0,0 +1,13 @@
+from ursina import * 
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+app = Ursina
+
+def update():
+    pass
+
+def timedown():
+
+timedown()
+
+app.run()
